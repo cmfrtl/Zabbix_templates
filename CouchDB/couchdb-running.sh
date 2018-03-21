fa89aaeca597585bac519fb1d8d30a49927f5fdc
Eloy Garcia <eloygn@users.noreply.github.com>
2018-03-21 09:50:47 +0000

    Added 123 items

--- CouchDB/couchdb-running.sh
+++ CouchDB/couchdb-running.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Check bigcouch service locally. This script needs to be copied into /etc/zabbix/scripts/
+# Check couchDB service locally. This script needs to be copied into /etc/zabbix/scripts/
 # Created by Eloy Garcia on March/2018
 
 value=$(curl -s "http://127.0.0.1:5984")
