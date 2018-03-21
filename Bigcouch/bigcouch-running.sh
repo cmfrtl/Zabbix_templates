a51afb79920a40da8bccaf9b81e927185bc1993d
Eloy Garcia <eloygn@users.noreply.github.com>
2018-03-21 09:36:50 +0000

    Check bigcouch service locally

---
+++ Bigcouch/bigcouch-running.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Check bigcouch service locally. This script needs to be copied into /etc/zabbix/scripts/
+# Created by Eloy Garcia on March/2018
+
+value=$(curl -s "http://127.0.0.1:5984")
+if [[ "$value" =~ Welcome ]]; then
+  echo '1'
+else
+  echo '0'
+f
